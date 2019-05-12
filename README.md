cd7a166ea5255cbfd02ed6d77d2445cd37021174
Ixl123 <cofman.jacob@gmx.de>
2019-05-12 14:28:46 +0200

    first try

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# vscodeaction
+# Vscode action to release a plugin
+
+This Action tries to release a vscode plugin via the [vsce](https://github.com/microsoft/vscode-vsce) tool.
+
+Make sure to pass a PUBLISH_TOKEN an explanation on how to register a token can be found [here](https://code.visualstudio.com/api/working-with-extensions/publishing-extension)
+
+## 🔑 Secrets
+
+- PUBLISH_TOKEN - Required to publish a release to vscode Marketplace
+
+## 💻 Examples
+
+Trigger a release
+
+```hcl
+
+workflow "Release Vscode plugin" {
+  on = "release"
+  resolves = ["Release Vscode"]
+}
+
+action "WebPageTestActions" {
+  secrets = [
+    "GITHUB_TOKEN",
+    "PUBLISH_TOKEN",
+  ]
+}
+
+```
---
+++ entrypoint.js
@@ -0,0 +1,73 @@
+// entrypoint.js
+import { exec } from 'child_process';
+
+const { Toolkit } = require("actions-toolkit");
+const tools = new Toolkit();
+
+// check pre-requirements
+if (!checkForMissingEnv) tools.exit.failure("Failed!");
+
+// run the script
+release();
+
+async function release() {
+    const publishToken = process.env.PUBLISH_TOKEN;
+    let { stdout, stderr } = await sh(`vsce publish -p ${publishToken}`);
+    if(stdout){
+        tools.exit.success(`Succesfully run! ${stdout}`);
+    }else{
+        tools.log.error(`Somehting went wrong ${stderr}`);
+    }
+}
+
+
+/**
+ * Execute shell command
+ * @param {String} cmd
+ * @return {Object} { stdout: String, stderr: String }
+ */
+async function sh(cmd) {
+  return new Promise(function (resolve, reject) {
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ stdout, stderr });
+      }
+    });
+  });
+}
+
+
+/**
+ * Log warnings to the console for missing environment variables
+ */
+function checkForMissingEnv() {
+  const requiredEnvVars = [
+    "HOME",
+    "GITHUB_WORKFLOW",
+    "GITHUB_ACTION",
+    "GITHUB_ACTOR",
+    "GITHUB_REPOSITORY",
+    "GITHUB_EVENT_NAME",
+    "GITHUB_EVENT_PATH",
+    "GITHUB_WORKSPACE",
+    "GITHUB_SHA",
+    "GITHUB_REF",
+    "GITHUB_TOKEN",
+    "VSCE_TOKEN",
+  ];
+
+  const requiredButMissing = requiredEnvVars.filter(
+    key => !process.env.hasOwnProperty(key)
+  );
+  if (requiredButMissing.length > 0) {
+    // This isn't being run inside of a GitHub Action environment!
+    const list = requiredButMissing.map(key => `- ${key}`).join("\n");
+    const warning = `There are environment variables missing from this runtime.\n${list}`;
+    tools.log.warn(warning);
+    return false;
+  } else {
+    return true;
+  }
+}
